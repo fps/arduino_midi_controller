67aa4011c21602e4c6791eedf0d90bf29ff56178
Florian Paul Schmidt <mista.tapas@gmx.net>
2016-10-20 09:12:32 +0200

    cleanup and comments

--- midi_controller.ino
+++ midi_controller.ino
@@ -5,10 +5,16 @@
 #include <midi_Settings.h>
 
 
-const int buttonPinBase = 3;     
+// The first pin of consecutive pins to attach our switches to
+const int buttonPinBase = 3;
 const int numberOfButtons = 5;
 
+// An array of ints to remember the button states from the last loop
+// so we can detect state transitions
 int lastButtonState[numberOfButtons] = { LOW, LOW, LOW, LOW, LOW };
+
+// An array of ints to remember the last CC we sent, so we can send 
+// the corresponding different one on the next press
 int lastMidiState[numberOfButtons] = { 0, 0, 0, 0, 0 };
 
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -16,8 +22,11 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 void setup() 
 {
   MIDI.begin(1);
-  // Serial.begin(9600);
-  Serial.begin(38400); // baud rate for mod duo
+
+  Serial.begin(38400);
+  
+  // We use INPUT_PULLUP to use the included pullup resistors 
+  // in the microcontroller
   for (int button = 0; button < numberOfButtons; ++button)
   {
     pinMode(buttonPinBase + button, INPUT_PULLUP);
@@ -25,18 +34,17 @@ void setup()
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  
   for (int button = 0; button < numberOfButtons; ++button)
   {
     int state = digitalRead(buttonPinBase + button);
-    
+
+    // Detect state transitions    
     if (state != lastButtonState[button]) 
     {
-      // Serial.print(button + buttonPinBase, DEC);
-      
+      // Going from low to high (on push)
       if (state == HIGH) 
       {
+        // Check the last CC value we sent so we can send the right one now
         if (lastMidiState[button] == 0)
         {
           MIDI.sendControlChange(button + 1, 127, 1);
@@ -50,6 +58,7 @@ void loop() {
       } 
     }
     
+    // Store away the current state for the transition detection
     lastButtonState[button] = state;
   }
   
